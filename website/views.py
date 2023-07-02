1c6bbf6bee311fad1e92b5f159bf638eec55d9c3
Roza-Afarin <afarin.roza@gmail.com>
2023-07-02 16:54:53 +0330

    render has been created
    template folder and html files have been added to website app and render to views

--- website/views.py
+++ website/views.py
@@ -9,10 +9,13 @@ from django.http import HttpResponse,JsonResponse
 #def json_test(request):
 #    return JsonResponse({'name':'roza','family':'afarin'})
 def home_index(request):
-    return HttpResponse('<h1>home page</h1>')
+    #return HttpResponse('<h1>home page</h1>')
+    return render(request,'website/index.html')
 
 def about_index(request):
-    return HttpResponse('<h1>about page</h1>')
+    #return HttpResponse('<h1>about page</h1>')
+    return render(request,'website/about.html')
 
 def contact_index(request):
-    return HttpResponse('<h1>contact page</h1>')
+    #return HttpResponse('<h1>contact page</h1>')
+    return render(request,'website/contact.html')
