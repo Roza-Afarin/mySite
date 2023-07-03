4e0214f596dc6153702ccc6c9a34201d3bc7af8f
Roza-Afarin <afarin.roza@gmail.com>
2023-07-03 10:39:03 +0330

    static and media
    directory for statics and media has been added

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 ###
+from django.conf import settings
+from django.conf.urls.static import static
 #from website.views import http_test, json_test
 
 urlpatterns = [
@@ -26,3 +28,6 @@ urlpatterns = [
     #path('website/',include('website.urls'))
     path('',include('website.urls'))# home page
 ]
+urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
