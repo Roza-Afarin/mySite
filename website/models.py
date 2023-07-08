842b99c7d1e49eb4f75750d9c145bface8f01b6a
Roza-Afarin <afarin.roza@gmail.com>
2023-07-08 19:34:12 +0330

    models have been added
    model post has been added to blog app and model contact has been added to website app

--- website/models.py
+++ website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class contact(models.Model):
+    name = models.CharField(max_length = 255)
+    email = models.EmailField()
+    subject = models.CharField(max_length = 255)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add = True)
+    updated_date = models.DateTimeField(auto_now = True)
