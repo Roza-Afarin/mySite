e13f094d27b2b8b094ee774efba978db79501718
Roza-Afarin <afarin.roza@gmail.com>
2023-07-10 18:09:14 +0330

    blog models have been shown in html file
    urls, views and html files in blog app have been changed

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('',blog_index,name='blog'),
-    path('single',blog_single,name='detail')
+    path('single',blog_single,name='detail'),
+    path('<str:name>/<str:family_name>/<str:age>',blog_test,name='test')
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse,JsonResponse
+from blog.models import post
 
 def blog_index(request):
-    return render(request,'blog/blog-home.html')#template
+    posts = post.objects.filter(status = 1)
+    context = {'posts':posts}
+    return render(request,'blog/blog-home.html',context)#template
 
 def blog_single(request):
     return render(request,'blog/blog-single.html')
+
+def blog_test(request,name,family_name,age):
+    #posts = post.objects.all()
+    #posts = post.objects.filter(status=1)
+    #context = {'posts':posts}
+    context = {'name':name,'family_name':family_name,'age':age}
+    return render(request,'test.html',context)
 
