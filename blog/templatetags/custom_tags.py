147f82329d47d98fc2cf5e0669838de1704b891f
Roza-Afarin <afarin.roza@gmail.com>
2023-07-26 12:05:40 -0400

    improve next and previous buttons

--- blog/templatetags/custom_tags.py
+++ blog/templatetags/custom_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from blog.models import post
 
 register = template.Library()
 
@@ -7,3 +8,20 @@ def subtract(value, arg):
     return value - arg
 
 #register.filter('subtract', subtract)
+
+@register.simple_tag(name='countP')
+def count_null_previous_status(previous):
+    count = 0
+    while not previous.status:
+        count+=1
+        previous = post.objects.get(id=(previous.id)-1)
+    return count
+
+@register.simple_tag(name='countN')
+def count_null_next_status(next):
+    count = 0
+    while not next.status:
+        count+=1
+        next = post.objects.get(id=(next.id)+1)
+    return count
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'website.apps.WebsiteConfig',
     'blog.apps.BlogConfig',
     'hitcount',
+    'mathfilters',
 ]
 
 MIDDLEWARE = [
