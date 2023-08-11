825e1063967fa83386ca85575174b31e48c1123d
Roza-Afarin <afarin.roza@gmail.com>
2023-08-11 10:33:45 -0400

    reset password

---
+++ mysite/setting/dev.py
@@ -0,0 +1,33 @@
+from mysite.settings import *
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-1m^8w+7cur)i3&)5wg5(&e&=7p+9+w*19%jm%c(qysl)l9l8m_'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+#sitemap
+SITE_ID = 2
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATIC_ROOT = BASE_DIR / 'static/'
+MEDIA_ROOT = BASE_DIR / 'media/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
+]
+
+X_FRAME_OPTIONS = "SAMEORIGIN"
--- mysite/settings.py
+++ mysite/settings.py
@@ -17,16 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1m^8w+7cur)i3&)5wg5(&e&=7p+9+w*19%jm%c(qysl)l9l8m_'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -54,8 +45,7 @@ INSTALLED_APPS = [
      'captcha',
 ]
 
-#sitemap
-SITE_ID = 2
+
 
 #robot
 ROBOTS_USE_SITEMAP = False
@@ -101,15 +91,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -148,13 +130,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 ###
-STATIC_ROOT = BASE_DIR / 'static/'
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "statics",
-]
+
 ###
 
 # Default primary key field type
@@ -177,7 +155,10 @@ AUTHENTICATION_BACKENDS = [
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.googlemail.com'
-EMAIL_PORT = 25
+EMAIL_PORT = 587
 EMAIL_HOST_USER = 'afarin.roza@gmail.com'
-EMAIL_HOST_PASSWORD = 'kxzttpbbewtegtmk'
+EMAIL_HOST_PASSWORD = 'jfyqyydfbutsrnmz'
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
+##
